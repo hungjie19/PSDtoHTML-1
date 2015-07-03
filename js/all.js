ce23209a755d4f6e070ca4e806cb1eefe58aee49
hungjie19 <hungjie19@gmail.com>
2015-07-04 01:42:08 +0800

    add bxslider, scroll to btn

--- js/all.js
+++ js/all.js
@@ -1,25 +1,61 @@
 $(document).ready(function() {
 	
-		/* some custom settings */
-
-		$('.iosslider').iosSlider({
-			snapToChildren: true,
-			desktopClickDrag: true,
-			infiniteSlider: true,
-			snapSlideCenter: true,
-			onSlideChange: slideChange,
-			autoSlideTransTimer: 1500,
-			autoSlide: true,
-			autoSlideTimer: 5000,
-			stageCSS: {
-				overflow: 'visible'
-			},
-			navPrevSelector: $('.prevButton'),
-			navNextSelector: $('.nextButton')
-		});
-	
+	// slide
+	$('.iosslider').iosSlider({
+		snapToChildren: true,
+		desktopClickDrag: true,
+		infiniteSlider: true,
+		snapSlideCenter: true,
+		onSlideChange: slideChange,
+		autoSlideTransTimer: 1500,
+		autoSlide: true,
+		autoSlideTimer: 5000,
+		stageCSS: {
+			overflow: 'visible'
+		},
+		navPrevSelector: $('.prevButton'),
+		navNextSelector: $('.nextButton')
 	});
+
+	// RECENT PRODUCTS slide controls
+	recentSlide = $('#recentSlider').bxSlider({
+					controls: false,
+					displaySlideQty: 1,
+					default: 1000,
+					easing : 'easeInOutQuint',
+					pager :false
+				});
+
+	// FEATURED PRODUCTS slide controls
+	featuedSlide = $('#featuedSlider').bxSlider({
+				controls: false,
+				displaySlideQty: 1,
+				default: 1000,
+				easing : 'easeInOutQuint',
+				pager :false
+			});
+
+	// btn gotoTop 
+	$('.btn_gotoTop').hide().click(function() {
+
+		$('html, body').animate({
+			scrollTop: 0
+		}, 'medium');
+	});
+});
+
+
+function slideChange(args) {
 	
-	function slideChange(args) {
-		
+}
+
+// scroll ? show : hide 
+$(window).bind('scroll', function(){
+	if($(this).scrollTop() > 1200) {
+		$(".btn_gotoTop").fadeIn(200);
 	}
+	else{
+		$(".btn_gotoTop").fadeOut(200);
+	}
+});
+
